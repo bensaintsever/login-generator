2d6f5b2ba87ae86791ae36a5dd0e6ec26b04b492
Benjamin Saint-Sever <saintseverbenjamin@gmail.com>
2016-04-15 13:56:15 +0200

    Checkstyle optimisé à 17 erreurs, finbugs : 0, ajout du gitignore, et ajout du package-info.java.

--- src/main/java/dcll/LoginService.java
+++ src/main/java/dcll/LoginService.java
@@ -7,61 +7,67 @@ import java.util.List;
 
 
 /**
- * Classe de service permettant la gestion des login
+ * Classe de service permettant la gestion des login.
  */
 public class LoginService {
 
-    private ArrayList<String> loginsExistants;
+    /**
+     * Liste de logins existant.
+     */
+    private final ArrayList<String> loginsExistants;
 
     /**
-     * Construit un login service a partir de la liste des logins existants
-     * @param loginsExistants
+     * Construit un login service a partir de la liste des logins existants.
+     *
+     * @param loginsexistants tableau contenant les logins exitants.
      */
-    public LoginService(String[] loginsExistants) {
-        this.loginsExistants = new ArrayList<String>(Arrays.asList(loginsExistants));
+    public LoginService(final String[] loginsexistants) {
+        this.loginsExistants = new
+                ArrayList<String>(Arrays.asList(loginsexistants));
         Collections.sort(this.loginsExistants);
     }
 
     /**
-     * Verifie si un login existe deja
+     * Verifie si un login existe deja.
      *
-     * @param login le login a verifier
-     * @return true si le login existe deja, false sinon
+     * @param login le login a verifier.
+     * @return true si le login existe deja, false sinon.
      */
-    public boolean loginExists(String login) {
+    public final boolean loginExists(final String login) {
         return loginsExistants.contains(login);
     }
 
     /**
-     * Ajoute un login a la liste des logins existants
-     * @param login le login a ajouter
+     * Ajoute un login a la liste des logins existants.
+     *
+     * @param login le login a ajouter.
      */
-    public void addLogin(String login) {
+    public final void addLogin(final String login) {
         loginsExistants.add(login);
     }
 
     /**
-     * Recherche tous les logins commençant par un meme prefixe
+     * Recherche tous les logins commençant par un meme prefixe.
      *
-     * @param pref le prefixe
-     * @return la liste des logins prefixes par <em>pref</em>
+     * @param pref le prefixe.
+     * @return la liste des logins prefixes par <em>pref</em>.
      */
-    public List<String> findAllLoginsStartingWith(String pref) {
+    public final List<String> findAllLoginsStartingWith(final String pref) {
         ArrayList<String> loginsPrefixes = new ArrayList<String>();
-        for (String login: loginsExistants) {
+        for (String login : loginsExistants) {
             if (login.startsWith(pref)) {
                 loginsPrefixes.add(login);
             }
         }
-        return  loginsPrefixes;
+        return loginsPrefixes;
     }
 
     /**
-     * Recherche tous les logins
+     * Recherche tous les logins.
      *
-     * @return la liste de tous les logins
+     * @return la liste de tous les logins.
      */
-    public List<String> findAllLogins() {
-        return  loginsExistants;
+    public final List<String> findAllLogins() {
+        return loginsExistants;
     }
 }
---
+++ src/main/java/dcll/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @author Benjamin Saint-Sever.
+ */
+package dcll;
